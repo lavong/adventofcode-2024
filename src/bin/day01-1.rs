75dea9c7361f23a8e186b706caef0a5ebaca1eee
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2024-12-01 08:31:47 +0100

    day01

--- src/bin/day01-1.rs
+++ src/bin/day01-1.rs
@@ -6,20 +6,17 @@ use itertools::Itertools;
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("src/bin/day01.txt")?;
 
-    let mut location_ids_left = Vec::new();
-    let mut location_ids_right = Vec::new();
-    input.lines().for_each(|line| {
-        let (n, m) = line.split_whitespace().next_tuple().unwrap();
-        location_ids_left.push(n.parse::<u32>().unwrap());
-        location_ids_right.push(m.parse::<u32>().unwrap());
-    });
+    let (left, right): (Vec<usize>, Vec<usize>) = input
+        .lines()
+        .filter_map(|line| line.split_whitespace().next_tuple())
+        .map(|(n, m)| (n.parse::<usize>().unwrap(), m.parse::<usize>().unwrap()))
+        .unzip();
 
-    location_ids_left.sort();
-    location_ids_right.sort();
-    let total_distance: u32 = location_ids_left
+    let total_distance: usize = left
         .iter()
-        .zip(location_ids_right)
-        .map(|(n, m)| n.abs_diff(m))
+        .sorted()
+        .zip(right.iter().sorted())
+        .map(|(n, m)| n.abs_diff(*m))
         .sum();
 
     println!("solution part 1: {total_distance}");
--- src/bin/day01-2.rs
+++ src/bin/day01-2.rs
@@ -6,17 +6,15 @@ use itertools::Itertools;
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("src/bin/day01.txt")?;
 
-    let mut location_ids_left = Vec::new();
-    let mut location_ids_right = Vec::new();
-    input.lines().for_each(|line| {
-        let (n, m) = line.split_whitespace().next_tuple().unwrap();
-        location_ids_left.push(n.parse::<usize>().unwrap());
-        location_ids_right.push(m.parse::<usize>().unwrap());
-    });
+    let (left, right): (Vec<usize>, Vec<usize>) = input
+        .lines()
+        .filter_map(|line| line.split_whitespace().next_tuple())
+        .map(|(n, m)| (n.parse::<usize>().unwrap(), m.parse::<usize>().unwrap()))
+        .unzip();
 
-    let similarity_score: usize = location_ids_left
+    let similarity_score: usize = left
         .iter()
-        .map(|n| n * location_ids_right.iter().filter(|m| *m == n).count())
+        .map(|n| n * right.iter().filter(|m| *m == n).count())
         .sum();
 
     println!("solution part 2: {similarity_score}");
