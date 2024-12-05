e3ba2de7dc675f08edac1df1b5cd88b6dedb7cf7
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2024-12-05 19:07:22 +0100

    day05

---
+++ src/bin/day05-1.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs;
+use std::io;
+
+use itertools::Itertools;
+
+fn main() -> io::Result<()> {
+    let input = fs::read_to_string("src/bin/day05.txt")?;
+    let (input_rules, input_updates) = input.split_once("\n\n").unwrap();
+
+    let rules = parse_rules(input_rules);
+    let updates = parse_updates(input_updates);
+
+    let valid_updates: u32 = (updates.iter())
+        .filter(|pages| pages.is_sorted_by(|n, m| applies(&rules, m, n)))
+        .map(|pages| pages[pages.len() / 2])
+        .sum();
+
+    println!("solution part 1: {valid_updates}");
+    Ok(())
+}
+
+fn parse_rules(input: &str) -> PageOrderingRules {
+    let mut rules = PageOrderingRules::new();
+    for rule in input.lines() {
+        let (n, m) = rule.split_once('|').unwrap();
+        rules
+            .entry(m.parse().unwrap())
+            .or_default()
+            .replace(n.parse().unwrap());
+    }
+    rules
+}
+
+fn parse_updates(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec()
+}
+
+type PageOrderingRules = HashMap<u32, HashSet<u32>>;
+
+fn applies(rules: &PageOrderingRules, k: &u32, v: &u32) -> bool {
+    rules.get(k).map(|rules| rules.contains(v)).unwrap_or(false)
+}
