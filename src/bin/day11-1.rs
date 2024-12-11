1c16241b312de63ec02f1293b30f7786185bc33a
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2024-12-11 07:15:26 +0100

    day11

---
+++ src/bin/day11-1.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io;
+
+use itertools::Itertools;
+
+fn main() -> io::Result<()> {
+    let input = fs::read_to_string("src/bin/day11.txt")?;
+
+    let mut stones = input
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect_vec();
+    (0..25).for_each(|_| {
+        let mut s: Vec<u64> = vec![];
+        for i in 0..stones.len() {
+            let n = stones[i];
+            let ns = n.to_string();
+            if n == 0 {
+                s.push(1);
+            } else if ns.len() % 2 == 0 {
+                let (x, y) = ns.split_at(ns.len() / 2);
+                s.push(x.parse::<u64>().unwrap());
+                s.push(y.parse::<u64>().unwrap());
+            } else {
+                s.push(n * 2024);
+            }
+        }
+        stones = s;
+    });
+    let stone_count = stones.len();
+
+    println!("solution part 1: {stone_count}");
+    Ok(())
+}
---
+++ src/bin/day11-2.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let input = fs::read_to_string("src/bin/day11.txt")?;
+
+    let mut stones = input
+        .split_whitespace()
+        .map(|n| (n.parse::<u64>().unwrap(), 1))
+        .collect::<HashMap<u64, u64>>();
+    (0..75).for_each(|_| {
+        let mut s: HashMap<u64, u64> = HashMap::new();
+        for (&n, &cache) in &stones {
+            if n == 0 {
+                *s.entry(1).or_default() += cache;
+            } else {
+                let digits = n.ilog10() + 1;
+                if digits % 2 == 0 {
+                    *s.entry(n % 10u64.pow(digits / 2)).or_default() += cache;
+                    *s.entry(n / 10u64.pow(digits / 2)).or_default() += cache;
+                } else {
+                    *s.entry(n * 2024).or_default() += cache;
+                }
+            }
+        }
+        stones = s;
+    });
+    let stone_count: u64 = stones.values().sum();
+
+    println!("solution part 2: {stone_count}");
+    Ok(())
+}
