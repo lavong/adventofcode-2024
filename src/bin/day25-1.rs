9e350fcbbbd13a2e4ba0c65184ab2031b064416b
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2024-12-25 20:30:44 +0100

    day25

--- src/bin/day25-1.rs
+++ src/bin/day25-1.rs
@@ -5,13 +5,28 @@ use itertools::Itertools;
 
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("src/bin/day25.txt")?;
+    let (keys, locks) = parse_input(&input);
 
+    let mut fitting_keylock_pairs = 0;
+    for key in &keys {
+        for lock in &locks {
+            if key.iter().zip(lock.iter()).all(|(k, l)| k + l < 6) {
+                fitting_keylock_pairs += 1;
+            }
+        }
+    }
+
+    println!("solution part 1: {fitting_keylock_pairs}");
+    Ok(())
+}
+
+fn parse_input(input: &String) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
     let mut keys = Vec::new();
     let mut locks = Vec::new();
     for kl in input.split("\n\n") {
         let keylock = kl.lines().map(|l| l.chars().collect_vec()).collect_vec();
         let mut heights = vec![0; keylock.len()];
-        for y in 0..keylock.len() {
+        for y in 1..(keylock.len() - 1) {
             for x in 0..keylock[y].len() {
                 if keylock[y][x] == '#' {
                     heights[x] += 1;
@@ -24,16 +39,5 @@ fn main() -> io::Result<()> {
             keys.push(heights);
         }
     }
-
-    let mut fitting_keylock_pairs = 0;
-    for key in &keys {
-        for lock in &locks {
-            if key.iter().zip(lock.iter()).all(|(k, l)| k + l < 8) {
-                fitting_keylock_pairs += 1;
-            }
-        }
-    }
-
-    println!("solution part 1: {fitting_keylock_pairs}");
-    Ok(())
+    (keys, locks)
 }
